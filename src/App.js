4dc357381e0e9e34c7e9c5fbba6c6052ca8af3f1
David San Antonio <davsan56@gmail.com>
2024-05-29 12:07:30 -0400

    Moving marker

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import "./App.css"
 import "leaflet/dist/leaflet.css"
 
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { useState } from "react";
+
+import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
 import { Icon } from "leaflet";
 
 import markerIconPng from 'leaflet/dist/images/marker-icon.png';
 import markerShadowPng from 'leaflet/dist/images/marker-shadow.png';
 
 function App() {
-  const markers = [
-    {
-      geocode: [48.86, 2.3522],
-      popUp: "Hello, I am pop up 1"
-    },
-    {
-      geocode: [48.85, 2.3522],
-      popUp: "Hello, I am pop up 2"
-    },
-    {
-      geocode: [48.855, 2.34],
-      popUp: "Hello, I am pop up 3"
-    }
-  ];
+  return (
+    <MyMapComponent />
+  );
+}
+
+function MyMapComponent() {
+
+  
+
+  return (
+    <MapContainer
+      center={[48.8566, 2.3522]}
+      zoom={13}
+    >
+      <MyClickComponent />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  )
+}
+
+function MyClickComponent() {
+  const [position, setPosition] = useState(null)
 
   const myIcon = new Icon({
     iconUrl: markerIconPng,
@@ -29,25 +42,21 @@ function App() {
     iconAnchor: [12, 41],
   })
 
+  const map = useMapEvents({
+    click: async (e) => {
+     setPosition(e.latlng)
+    }
+  })
   return (
-    <MapContainer
-      center={[48.8566, 2.3522]}
-      zoom={13}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <>
+    {position && (
+      <Marker
+        position={position}
+        icon={myIcon}
       />
-      {markers.map(marker => (
-        <Marker
-          position={marker.geocode}
-          icon={myIcon}
-         />
-
-        
-      ))}
-    </MapContainer>
-  );
+    )}
+    </>
+  )
 }
 
 export default App;
