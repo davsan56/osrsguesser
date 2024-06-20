88c793b9f84db0f4a20c634192c656a528855297
David San Antonio <davsan56@gmail.com>
2024-06-20 16:19:49 -0400

    Formatting countdown

--- src/components/HomePageContainer.js
+++ src/components/HomePageContainer.js
@@ -3,6 +3,20 @@ import Countdown from "react-countdown";
 import RoundScores from "../components/RoundScores";
 
 function HomePageContainer({ roundScores, numberOfGamesPlayed }) {
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <p>Refresh to play!</p>;
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          {hours}:{minutes}:{seconds}
+        </span>
+      );
+    }
+  };
+
   return (
     <>
       <p>Todays scores</p>
@@ -15,6 +29,7 @@ function HomePageContainer({ roundScores, numberOfGamesPlayed }) {
         date={
           Date.now() + (86400 - (Math.floor(new Date() / 1000) % 86400)) * 1000
         }
+        renderer={renderer}
       />
     </>
   );
