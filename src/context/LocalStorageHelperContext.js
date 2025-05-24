8729cd11a18083631ffa30ff16bc41e58d9f92b1
David San Antonio <davsan56@gmail.com>
2025-05-24 12:17:27 -0400

    Removing comment

--- src/context/LocalStorageHelperContext.js
+++ src/context/LocalStorageHelperContext.js
@@ -4,8 +4,6 @@ import * as LocalStorageHelper from "../data/LocalStorageHelper";
 const LocalStorageHelperContext = createContext();
 
 export function LocalStorageHelperProvider({ children }) {
-  // You can add logic here to inject dependencies, e.g., game mode, if needed
-
   return (
     <LocalStorageHelperContext.Provider value={LocalStorageHelper}>
       {children}
